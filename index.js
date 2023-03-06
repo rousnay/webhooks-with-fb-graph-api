86b19932fe228317ecdf8b06c6ee038686626570
Rousnay <mr.rousnay@gmail.com>
2023-03-06 18:40:15 +0600

    Testing API4

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get(["/facebook", "/instagram"], function (req, res) {
 });
 
 app.post("/facebook", function (req, res) {
-  console.log("Facebook request body:", req.body.text());
+  console.log("Facebook request body:", req.body.json());
 
   if (!req.isXHubValid()) {
     console.log(
